096048b9e730a0354d2f810850ee30e182bc1ae9
natalybarah <nathalybs2021@outlook.es>
2026-05-11 07:42:24 -0700

    seed with 20 mock cases and hearings

---
+++ backend/seed.py
@@ -0,0 +1,35 @@
+import os 
+from datetime import date
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from supabase import create_client, Client 
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_SERVICE_KEY")
+supabase: Client = create_client(url, key)
+
+
+
+cases_response=(
+    supabase.table("cases")
+    .select("*")
+    .execute()
+)
+
+
+case_id_map={}
+for case in cases_response.data:
+    case_id_map[case["filevine_project_id"]]= case["id"]
+
+for hearing in hearings:
+    hearing["case_id"]= case_id_map[hearing["case_id"]]
+
+
+hearings_response= (
+    supabase.table("hearings")
+    .insert(hearings)
+    .execute()
+)
+
