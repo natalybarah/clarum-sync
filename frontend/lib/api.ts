5aae97ba2f27efc88f4ab7753d4c656655e24e16
natalybarah <nathalybs2021@outlook.es>
2026-05-24 18:50:41 -0700

    fastapi url

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,8 +1,9 @@
-const FAST_API_URL= "http://127.0.0.1:8000"
+const FAST_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"
 
 export const getCases= async() =>{
+    console.log('API URL:', FAST_API_URL)
     const data= await fetch(`${FAST_API_URL}/cases`, { cache: 'no-store' })
-    if (!data.ok) throw new Error("An error ocurred, failed to fetch cases")
+    if (!data.ok) throw new Error(`An error ocurred, failed to fetch cases ${data.status} ${data.statusText}`)
     const cases= await data.json()
     return cases
 }
