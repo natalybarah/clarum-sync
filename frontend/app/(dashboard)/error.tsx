33b4e28ba6b54571fafdf69ccd430a476dd4b0c3
natalybarah <nathalybs2021@outlook.es>
2026-05-24 18:37:51 -0700

    no store cache in API endpoints

---
+++ frontend/app/(dashboard)/error.tsx
@@ -0,0 +1,12 @@
+
+'use client'
+
+export default function Error({ error }: { error: Error }) {
+    console.log(error, "error")
+  return (
+    <div>
+      <h2>Something went wrong</h2>
+      <p>{error.message}</p>
+    </div>
+  )
+}
--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,21 +1,21 @@
 const FAST_API_URL= "http://127.0.0.1:8000"
 
 export const getCases= async() =>{
-    const data= await fetch(`${FAST_API_URL}/cases`)
+    const data= await fetch(`${FAST_API_URL}/cases`, { cache: 'no-store' })
     if (!data.ok) throw new Error("An error ocurred, failed to fetch cases")
     const cases= await data.json()
     return cases
 }
 
 export const getNotices= async()=>{
-    const data= await fetch(`${FAST_API_URL}/notices`)
+    const data= await fetch(`${FAST_API_URL}/notices`, { cache: 'no-store' })
     if (!data.ok) throw new Error("An error ocurred, failed to fetch notices")
     const notices= data.json()
     return notices
 }
 
 export const getGapCases= async()=>{
-    const data= await fetch(`${FAST_API_URL}/cases/gaps`)
+    const data= await fetch(`${FAST_API_URL}/cases/gaps`, { cache: 'no-store' })
     if (!data.ok) throw new Error("An error ocurred, failed to fetch cases with gaps")
     const gapCases= data.json()
     return gapCases
